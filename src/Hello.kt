6afbbb6a3ebfbadc6e85ea0412ecf4a0b5e26673
Tarazi <200103456@stu.sdu.edu.kz>
2022-09-19 14:02:18 +0600

    1.4

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,4 +1,5 @@
 fun main(args:Array<String>){
-    val isUnit = println("This is an expression")
-    println(isUnit)
+    val temperature = 10
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+    println(message)
 }
